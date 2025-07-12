e64698040aee5edeb77f3bfaf5093d351595b739
Str1de <34148467+Str1de@users.noreply.github.com>
2025-07-12 15:42:05 -0500

    f
    f

---
+++ resources/[standalone]/karma-minimap/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+this_is_a_map 'yes'
+lua54 'yes'
+games {'gta5'}
+author 'ANTUNES'
+description 'Minimap by Karma Developments 4.0'
+url 'https://discord.gg/rw2gbsKXgG'
+
+client_script 'client.lua'
+dependency '/assetpacks'
